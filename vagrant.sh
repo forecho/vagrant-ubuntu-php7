bd3a66fca163eb72d585daa3e01ef0b15a314dc5
蔡正海 <caizhenghai@gmail.com>
2017-01-09 10:05:04 +0800

    去掉composer-setup.php hash_file 校验

--- vagrant.sh
+++ vagrant.sh
@@ -48,7 +48,6 @@ apt-get install -y php7.0 php7.0-fpm php7.0-mysql php-mongodb php-redis php7.0-c
 
 # install composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
